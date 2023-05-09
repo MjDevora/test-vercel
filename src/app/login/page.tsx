0157977990309f655988f93ef607a441bc5e488a
Hector Carrillo <technik.und.spiel@gmail.com>
2023-05-09 14:52:41 -0600

    google login button component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,12 @@
+'use client';
+
 import React from 'react';
+import Image from 'next/image';
+import {LoginWithGoogleButton} from '../components/login/login-with-google-button'
 import { useSupabase } from '../supabase-provider';
 
 export default function Login() {
     const {supabase} = useSupabase();
-    
-    const handleLogout = async () => {
-    await supabase.auth.signOut();
-    };
 
     async function signInWithGoogle() {
     const { data, error } = await supabase.auth.signInWithOAuth({
@@ -15,5 +15,21 @@ export default function Login() {
     console.log(data, error)
     }
 
-    return ()
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    return (
+        <div>
+            <h1 className="text-6xl font-bold text-center">
+                <a> Iniciar Sesion </a>
+            </h1>
+            {/*  centered button */}
+
+            <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => signInWithGoogle()}
+            >
+                <LoginWithGoogleButton/>
+            </button>
+
+        </div>
+    )
+
+}
