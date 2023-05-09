882d1eb9ee6c716b27a22a230b755b93d4ed236a
Hector Carrillo <technik.und.spiel@gmail.com>
2023-05-09 15:32:09 -0600

    sign in button done

--- src/app/components/login/login-with-google-button.tsx
+++ src/app/components/login/login-with-google-button.tsx
@@ -8,5 +8,8 @@ export interface LoginWithGoogleButtonProps {
 export const LoginWithGoogleButton: React.FC<LoginWithGoogleButtonProps> = ({ className = '' }) => (
     <div className={`${className} login-button`}>
         <span className="login-span">
-            <img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png" className="login-button-img" />Usar Google</span></div>
+            <img src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png" className="login-button-img" />
+                Usar Google
+        </span>
+    </div>
 );
