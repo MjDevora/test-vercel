76be35eaab4b1656a932dc77f916c4a285b60c26
Hector Carrillo <technik.und.spiel@gmail.com>
2023-05-09 23:08:08 -0600

    success modal removed from login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { useSupabase } from '../supabase-provider';
 import { LoginCard } from '../components/login-card/login-card';
-import { Success } from '../components/success/success';
 
 export default function Login() {
     const {supabase} = useSupabase();
@@ -20,9 +19,6 @@ export default function Login() {
         // center the login card at the center of the page
         <div className="flex min-h-screen flex-col items-center justify-center p-24">
            <LoginCard loginHandler={signInWithGoogle} /> 
-            <Success 
-                message = "¡Listo! Ya puedes usar tu cuenta de Google para iniciar sesión."
-            />
         </div>
     )
 
